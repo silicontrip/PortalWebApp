81f0e618f6aa6a709db8e11b4e48d0ed456f0405
silicontrip <silicontrip@gmail.com>
2019-03-30 20:47:48 +1100

    so far so good.

---
+++ src/PortalDAO.java
@@ -0,0 +1,19 @@
+package net.silicontrip.ingress;
+
+import java.util.ArrayList;
+import com.google.common.geometry.*;
+
+public interface PortalDAO {
+
+	public ArrayList<Portal> getInRegion (S2Region reg) throws PortalDAOException;
+	public S2LatLng getLocation (String s) throws PortalDAOException;
+	public S2LatLng getLocationFromTitle (String title) throws PortalDAOException;
+	public S2LatLng getLocationFromGuid (String Guid) throws PortalDAOException;
+	public S2LatLng getLocationFromLocation (long latE6, long lngE6) throws PortalDAOException;
+	public void delete (String guid) throws PortalDAOException;
+	public void updateTitle(String guid, String title) throws PortalDAOException;
+	public void updateLocation(String guid, long latE6, long lngE6) throws PortalDAOException;
+	public void insert(String guid, String title, long latE6, long lngE6) throws PortalDAOException;
+
+}
+	

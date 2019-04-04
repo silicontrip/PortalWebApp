8a8dbca9d2a487fa949a32062be8cf6d50c611e7
silicontrip <silicontrip@gmail.com>
2019-04-04 21:52:06 +1100

    Added toString for debug.

--- src/AreaDistribution.java
+++ src/AreaDistribution.java
@@ -5,4 +5,12 @@ public class AreaDistribution {
 	// making these public cbf writing get/set
 	public UniformDistribution mu;
 	public Double area;
+
+	@Override
+	public String toString() { 
+		if (mu!=null)
+			return "" + mu.toString() +": " +area; 
+		else
+			return "[,]: " +area;
+	}
 }

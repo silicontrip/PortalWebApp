6f0d154b056136365f9da2a56468bf33ff6fca19
silicontrip <silicontrip@gmail.com>
2019-04-12 01:55:42 +1000

    migrated this code into single EntityDAO

--- src/MUCellDAO.java
+++
@@ -1,8 +0,0 @@
-package net.silicontrip.ingress;
-import net.silicontrip.UniformDistribution;
-import java.util.HashMap;
-import com.google.common.geometry.S2CellId;
-public interface MUCellDAO{
-	public HashMap<S2CellId,UniformDistribution> getAll() throws MUCellDAOException;
-	public void updateAll(HashMap<S2CellId,UniformDistribution> cellmu) throws MUCellDAOException;
-}

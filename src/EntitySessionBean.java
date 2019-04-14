94b2c7ec1d137ae0c867043f6dab4920f47935e6
silicontrip <silicontrip@gmail.com>
2019-04-15 03:17:06 +1000

    Entity EJB abstracts the DAO from the Servlets.

---
+++ src/EntitySessionBean.java
@@ -0,0 +1,107 @@
+/**
+* The EntitySessionBean handles all the requests for DAO entities
+*
+* @author  Silicon Tripper
+* @version 1.0
+* @since   2019-03-31
+*/
+package net.silicontrip.ingress;
+
+import java.util.ArrayList;
+
+import javax.ejb.Stateless;
+import net.silicontrip.*;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.common.geometry.*;
+
+@Stateless
+
+public class EntitySessionBean {
+
+	private EntityDAO dao = null;
+
+	private EntityDAO getDAO() {
+		if (dao==null)
+			dao = new SQLEntityDAO();
+		return dao;
+	}
+	
+/**
+ * get the S2LatLng location of a portal description.
+ *
+ *This is a conditional logic method which compares the description with a few known formats
+ *and calls the appropriate method to determine the location.
+ *
+ * @param s the description of a portals location.
+ *
+ * @return S2LatLng of the location.
+ *
+ */
+	public S2LatLng getPortalLocation(String s)
+	{
+		if (s==null)
+			return null;
+		if (s.matches("^[0-9a-fA-F]{32}\\.1[16]$"))
+			return getDAO().getPortalLocationFromGuid(s);
+		if (s.matches("(\\+|-)?([0-9]+(\\.[0-9]+)),(\\+|-)?([0-9]+(\\.[0-9]+))"))
+		{
+			String[] ll = s.split(",");
+			Double lat = Double.parseDouble(ll[0]);
+			Double lng = Double.parseDouble(ll[1]);
+			long latE6 = Math.round(lat * 1000000);
+			long lngE6 = Math.round(lng * 1000000);
+
+			return S2LatLng.fromE6(latE6,lngE6);
+		
+			//return getDAO().getPortalLocationFromLocation(latE6,lngE6);
+		}
+		return getDAO().getPortalLocationFromTitle(s);
+	}
+	/**
+ * Gets all Portals within an S2Region.
+ *
+ * @param reg S2Region describing the area which contains the portals
+ *
+ * @return ArrayList of the portals
+ *
+ */
+	public ArrayList<Portal> getPortalInRegion(S2Region reg)
+	{
+		ArrayList<Portal> ret = new ArrayList<Portal>();
+
+		ArrayList<Portal> rectPortals = getDAO().getPortalsInRect(reg.getRectBound());
+		for (Portal p : rectPortals) {
+			long plat = p.getLatE6();
+			long plng = p.getLngE6();
+			
+			S2LatLng latLng = p.getS2LatLng();
+			S2Cell cell = new S2Cell(latLng); // I wonder what level cell this produces
+			if (reg.contains(cell))
+			{
+				ret.add(p);
+			}
+		}
+		return ret;
+	}
+	
+	public ArrayList<Link> getLinkInRect(S2LatLngRect rect)
+	{
+		ArrayList<Link> all  = getDAO().getLinkAll();
+		ArrayList<Link> ret = new ArrayList<Link>();
+		// should I just copy the getAll method to save memory.
+		for (Link l : all)
+		{
+			S2LatLngRect lb = l.getBounds();
+			if (rect.contains(lb) || rect.intersects(lb))
+				ret.add(l);
+		}
+		return ret;
+	}
+	public ArrayList<Link> getLinkAll()
+	{
+			return getDAO().getLinkAll();
+	}
+	
+
+}

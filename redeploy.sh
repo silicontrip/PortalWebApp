861eba4d5b63aa45ac0ef9a70bc83aea2330653b
silicontrip <silicontrip@gmail.com>
2019-04-22 20:19:03 +1000

    Moved glassfish off the (small) root drive.

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/local/glassfish4/bin/asadmin undeploy portalApi 
-/usr/local/glassfish4/bin/asadmin deploy dist/portalApi.war
+/Volumes/Octant/glassfish4/bin/asadmin undeploy portalApi 
+/Volumes/Octant/glassfish4/bin/asadmin deploy dist/portalApi.war

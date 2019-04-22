fc4f680ed5cef8c3d24d5051293f5179b54a95c1
silicontrip <silicontrip@gmail.com>
2019-04-22 20:26:04 +1000

    Moved field methods to fieldBean, and now using the CellPersistenceEntity

--- src/net/silicontrip/ingress/CellSessionBean.java
+++ src/net/silicontrip/ingress/CellSessionBean.java
@@ -9,63 +9,19 @@ package net.silicontrip.ingress;
 
 import com.google.common.geometry.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.jms.*;
-import javax.naming.*;
-import net.silicontrip.AreaDistribution;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import net.silicontrip.UniformDistribution;
 
 @Stateless
-
+@LocalBean
 public class CellSessionBean {
 
-	private HashMap<S2CellId, UniformDistribution> cellmu = null;
-	private EntityDAO dao = null;
-	/** threshold for MU rounding */
-	public double range = 0.5; 
-        private QueueConnectionFactory qcf = null;
-        private InitialContext ctx = null;
-        private QueueConnection queueCon = null;
-        private QueueSession queueSession = null;
-        private Queue submitQueue = null;
-	private QueueSender sender = null;
-
-	private void initCellQueue() throws NamingException, JMSException  {
-		if (ctx == null) 
-		{
-			ctx = new InitialContext();
-                        qcf = (QueueConnectionFactory) ctx.lookup("jms/QueueConnectionFactory");
-                        queueCon = qcf.createQueueConnection();
-                        queueSession = queueCon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-			submitQueue = (Queue)ctx.lookup("jms/cellQueue");
-			sender = queueSession.createSender(submitQueue);
-		}
-	}
-
-	private EntityDAO getDAO() {
-		if (dao==null)
-			dao = new SQLEntityDAO();
-		return dao;
-	}
-
-	/**
-   * This will return the mu uniform distribution for
-   * the requested S2CellId.  The S2CellId HashMap is quite small
-   * so is cached here for speed, rather than querying the DAO each time.
-   * @param cell The S2CellId for which the MU is required
-   * @return UniformDistribution of the MU. May be null if no information
-   * exists for the specified cell.
-   */
-
-	public UniformDistribution getMU(S2CellId cell) {
-		if (cellmu != null)
-			return cellmu.get(cell);
-
-		cellmu = getDAO().getMUAll();
-		return cellmu.get(cell);
-	}
+	@PersistenceContext(unitName="net.silicontrip.ingress.persistence")
+	EntityManager em;
+	
 	/**
 	 * Returns an S2Polygon of the requested cell.
    * @param cell The S2CellId for which the S2Polygon is required.
@@ -135,252 +91,42 @@ public class CellSessionBean {
 		rc.setMaxCells(20);
 		return rc.getCovering (thisField);
 	}
-	/**
-	 * Generates an intersection map of a Field and its cells.
-	 * Returns a HashMap of the S2CellId and its area in Km^2 and the min and max MU for that cell.
-	 * @param thisField the S2Polygon
-	 * @return HashMap containing the S2CellId with its Area and Uniform Distribution
+
+		/**
+	 * Get the area of the intersection between a polygon and cell.
+	 * @param field the field polygon
+	 * @param cell the cell polygon
+	 * @return Double of the area in km^2.
 	 */
-	public HashMap<S2CellId,AreaDistribution> getIntersectionMU(S2Polygon thisField)
-	{
-
-		HashMap<S2CellId,AreaDistribution> response = new HashMap<S2CellId,AreaDistribution>();
-
-		for (S2CellId cell: getCellsForField(thisField))
-		{
-		//System.out.println("getIntersectionMU: " + cell.toToken());
-			S2Polygon intPoly = new S2Polygon();
-			S2Polygon cellPoly = getS2Polygon(new S2Cell(cell));
-
-			intPoly.initToIntersection(thisField, cellPoly);
-			AreaDistribution ad = new AreaDistribution();
-			ad.area = intPoly.getArea() * 6367 * 6367 ;
-			ad.mu = getMU(cell);
-
-			response.put(cell,ad);
-		}
-		return response;
-	}
-	/**
-	 * find the MU for a known field
-	 *
-	 * @param field the field to search for
-	 *
-	 * @return int of the known mu or -1 if unknown
-	 */
-	public int muKnownField(Field field)
-	{
-		for (Field f : getDAO().findField(field))
-			return f.getMU();
-		return -1;
-	}
-	/**
-	 * Determines if a field's mu is possibly valid.
-	 * Requires adequate cell data for the field being tested
-	 * if not enough cell data is available this method will return true.
-	 * @param s2Field The field to test.
-	 * @param mu the integer value of the field. Fields have floating point mu values but are rounded to the nearest integer greater than 0
-	 * @return boolean indicating if the MU specified agrees with the cell data.
-	 */
-	public boolean muFieldValid(S2Polygon s2Field,int mu)
-	{
-		UniformDistribution muest = muForField(s2Field);
-		if (muest.getLower() == -1)
-			return true;
-
-		return muest.roundAboveZero().contains(mu);
-	}
-	/**
-	 * Get the MU UniformDistribution for a field.
-	 * @param s2Field the field to calculate its mu
-	 * @return UniformDistribution of the mu.
-	 */
-	public UniformDistribution muForField(S2Polygon s2Field)
-	{
-
-		//S2CellUnion cells = getCellsForField(s2Field);
-		HashMap<S2CellId,AreaDistribution> muArea = getIntersectionMU(s2Field);
-		UniformDistribution fieldmu = new UniformDistribution(0,0);
-
-		for (Map.Entry<S2CellId,AreaDistribution> entry : muArea.entrySet())
-		{
-			AreaDistribution cell = entry.getValue();
-			//System.out.println(cell);
-			// do we have information for this cell
-			// if not we should skip this whole thing
-			if (cell.mu!=null)
-			{
-				UniformDistribution uniformMU = new UniformDistribution(cell.mu);
-				uniformMU = uniformMU.mul(cell.area);
-				fieldmu = fieldmu.add(uniformMU);
-			} else {
-				return new UniformDistribution(-1,-1);
-			}
-		}
-		return fieldmu;
-	}
-	/**
-	 * Checks the DAO to see if this field exists.
-	 * @param guid the guid of the field to check
-	 * @return boolean if the field exists or not
-	 */
-	public boolean hasFieldGuid(String guid)
-	{
-		return getDAO().existsField(guid);
-	}
-
-	/**
-	 * inserts the field using the DAO.
-	 * @param field the field entity to insert
-	 * @param valid the mu validity flag
-	 */
-	public void submitField(Field field, boolean valid)
-	{
-		getDAO().insertField(field.getCreator(),
-					field.getAgent(),
-					field.getMU(),
-					field.getGuid(),
-					field.getTimestamp(),
-					field.getTeam(),
-					field.getPGuid1(),
-					field.getPLat1(),
-					field.getPLng1(),
-					field.getPGuid2(),
-					field.getPLat2(),
-					field.getPLng2(),
-					field.getPGuid3(),
-					field.getPLat3(),
-					field.getPLng3(),
-					valid);
-		S2CellUnion fieldCells = getCellsForField(field.getS2Polygon());
-		getDAO().insertCellsForField(field.getGuid(),fieldCells);
-		// too easy?
-		// should this be here or in the caller?
-		//System.out.println("Valid for process? " + valid);
-		if (valid)
-		{
-		//	System.out.println("Process Field...");
-			processField(field);
-		}
-	}
-
-	private static Double getIntersectionArea(S2Polygon field, S2CellId cell)
+	
+	public static Double getIntersectionArea(S2Polygon field, S2CellId cell)
 	{
 		S2Polygon intPoly = new S2Polygon();
 		S2Polygon cellPoly = getS2Polygon(new S2Cell(cell));
 		intPoly.initToIntersection(field, cellPoly);
 		return intPoly.getArea() * 6367 * 6367 ;
 	}
-	/**
-	 * This is the MU worker method converting fields into cell mu
-	 * Any cells which have been modified are placed into the cellQueue
-	 * which then resubmits any fields which use that cell.
-	 * Proof of concept for realtime cell updating. I'm excited.
-	 * @param field the field entity to process into cells
-	 */
-	public void processField(Field field)
-	{
-		Integer score = field.getMU();
-		S2Polygon thisField = field.getS2Polygon();
-		S2CellUnion cells = getCellsForField(thisField);
-		Double area;
+	
+				/**
+   * This will return the mu uniform distribution for
+   * the requested S2CellId.  The S2CellId HashMap is quite small
+   * so is cached here for speed, rather than querying the DAO each time.
+   * @param cell The S2CellId for which the MU is required
+   * @return UniformDistribution of the MU. May be null if no information
+   * exists for the specified cell.
+   */
 
-/*
-		System.out.print ("Processing Cells: ");
-		for (S2CellId cello: cells) 
-			System.out.print (cello.toToken() + ", ");
-		System.out.println(".");
-*/
-
-
-		UniformDistribution initialMU = new UniformDistribution(score,range);
-
-		// an algorithmic version of the following equation
-		// mu[cell] = ( MU - intersectionArea[cell1] x mu[cell1] ... - intersectionArea[cellN] x mu[cellN]) / intersectionArea[cell]
-
-		for (S2CellId cellOuter: cells) {
-			StringBuilder cellLog = new StringBuilder();
-			UniformDistribution mus = new UniformDistribution(initialMU);
-			cellLog.append("( ");
-			cellLog.append(mus.toString());
-			for (S2CellId cellInner: cells) {
-				// if not cell from outer loop
-				if (!cellOuter.equals(cellInner))
-				{
-					double areaInner = getIntersectionArea(thisField,cellInner);
-					UniformDistribution cellInnerMU = getMU(cellInner);
-
-					cellLog.append(" - ");
-					cellLog.append(areaInner);
-					cellLog.append(" x ");
-					cellLog.append(cellInner.toToken());
-					cellLog.append(":");
-
-					if (cellInnerMU != null)
-					{
-						cellLog.append(cellInnerMU);
-						UniformDistribution cma = cellInnerMU.mul(areaInner);
-						mus = mus.sub(cma);
-					}
-					else
-					{
-						cellLog.append("undefined");
-						mus.setLower(0.0);
-					}
-				}
-			}
-			cellLog.append(" ) / ");
-			double areaOuter = getIntersectionArea(thisField,cellOuter);
-			cellLog.append(areaOuter);
-			mus=mus.div(areaOuter);
-			cellLog.insert(0," = ");
-			cellLog.insert(0,mus);
-			mus.clampLower(0.0);
-
-
-			UniformDistribution cellOuterMU = getMU(cellOuter);
-			cellLog.insert(0," <=> ");
-			cellLog.insert(0,cellOuterMU.toString());
-			cellLog.insert(0,": ");
-			cellLog.insert(0,cellOuter.toToken());
-		//	System.out.println(cellLog.toString());
-
-			// refine UD:cello with mus
-
-			if (cellOuterMU == null)
-			{
-				cellOuterMU = mus;
-			}
-			else
-			{
-				try {
-					if (cellOuterMU.refine(mus)){
-						System.out.println("" + cellOuter.toToken() + "->" + cellOuterMU.toString());
-						initCellQueue();
-						Message msg = queueSession.createTextMessage(cellOuter.toToken());
-						sender.send(msg);
-						// add cell to modified array
-						// nah, just dump it straight on the cell queue. (might be a bit premature MU hasn't been saved)
-					}
-				} catch (ArithmeticException | JMSException | NamingException e) {
-					; // something something, out of range error
-				}
-			}
-
-			// end refine UD:cello
-			//multi.put(cello,cellomu);
-		}
-
-		// push modified cell array into jms/cellQueue
+	public UniformDistribution getMU(S2CellId cell) {
 		
-	/*
-		for (S2CellId cell: multi.keySet())
-		{
-			response.put(cell.toToken(), multi.get(cell).getArrayList());
-			putMU(cell, multi.get(cell));
-		}
-		return response;
-		*/
+		//EntityManager em = emf.createEntityManager();
+
+	// it's always satisfying to comment out debug code, when you know it's now working.
+	//	System.out.println("Cell: "+ cell.toToken() + " : " + cell.id());
+		
+		CellMUEntity cellmu = em.find(CellMUEntity.class, cell.id());
+		
+		if (cellmu==null)
+			return null;
+		return cellmu.getDistribution();
 	}
 }
-

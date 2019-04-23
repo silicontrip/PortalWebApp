1b679f0ef3bf926f97872af9bf2d0f21e2ca102b
silicontrip <silicontrip@gmail.com>
2019-04-23 23:49:35 +1000

    empty the queues.

---
+++ flush-queue.sh
@@ -0,0 +1,4 @@
+/Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue CellQueue
+/Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue FieldQueue
+/Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue LinkQueue
+/Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue PortalQueue

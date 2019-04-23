99a484556451804f9121d97de4071a41fc3b0696
silicontrip <silicontrip@gmail.com>
2019-04-23 23:48:14 +1000

    fixed null image error.

--- src/net/silicontrip/ingress/PortalQueueMDB.java
+++ src/net/silicontrip/ingress/PortalQueueMDB.java
@@ -39,7 +39,11 @@ public class PortalQueueMDB implements MessageListener {
 				// grr sometimes these contain nulls.  
 				// have to go over each with a fine grade if statement.
 				//System.out.println("" + pobj.getString("image").length() + " / " + pobj.getString("image"));
-				dao.writePortalFull(pobj.getString("guid"), pobj.getString("title"), pobj.getLong("latE6"), pobj.getLong("lngE6"),pobj.getString("team"),pobj.getInt("level"),pobj.getInt("resCount"),pobj.getInt("health"),pobj.getString("image"));
+				//Object image = pobj.getObject("image");
+				if (!pobj.isNull("image"))
+					dao.writePortalFull(pobj.getString("guid"), pobj.getString("title"), pobj.getLong("latE6"), pobj.getLong("lngE6"),pobj.getString("team"),pobj.getInt("level"),pobj.getInt("resCount"),pobj.getInt("health"),pobj.getString("image"));
+				else
+					dao.writePortalFull(pobj.getString("guid"), pobj.getString("title"), pobj.getLong("latE6"), pobj.getLong("lngE6"),pobj.getString("team"),pobj.getInt("level"),pobj.getInt("resCount"),pobj.getInt("health"),"");	
 			}
 			else 
 				dao.writePortal(pobj.getString("guid"), pobj.getLong("latE6"), pobj.getLong("lngE6"),pobj.getString("team"));

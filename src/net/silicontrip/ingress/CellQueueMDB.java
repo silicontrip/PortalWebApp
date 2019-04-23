0693b753734f590e4491f0d9fb37a21f9ed2f6ef
silicontrip <silicontrip@gmail.com>
2019-04-23 13:01:09 +1000

    Changed from Cell re-processor to field re-processor.  Still needs major overhaul.

--- src/net/silicontrip/ingress/CellQueueMDB.java
+++ src/net/silicontrip/ingress/CellQueueMDB.java
@@ -24,13 +24,17 @@ public class CellQueueMDB implements MessageListener {
 		@EJB
 		private FieldSessionBean fieldBean;
 
+		@EJB
+		private SQLEntityDAO dao;
+		
 		@Override
 		public void onMessage(Message message) {
 			TextMessage textMessage = (TextMessage) message;
 			try {
-				System.out.println("Queue: " + textMessage.getText());
-				S2CellId pcell = S2CellId.fromToken(textMessage.getText());
-				fieldBean.processCell(pcell);
+				// System.out.println("Queue: " + textMessage.getText());
+				//S2CellId pcell = S2CellId.fromToken(textMessage.getText());
+				Field fi = dao.getField(textMessage.getText());
+				fieldBean.processField(fi);
 
 			} catch (JMSException ex) {
 				Logger.getLogger(CellQueueMDB.class.getName()).log(Level.SEVERE, null, ex);

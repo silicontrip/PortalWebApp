6ad43e43e62f50ef0c3e41dcc4925fbb0d9e0a0c
silicontrip <silicontrip@gmail.com>
2019-05-03 20:45:00 +1000

    stop application before flushing queue.

--- flush-queue.sh
+++ flush-queue.sh
@@ -1,4 +1,6 @@
+/Volumes/Octant/glassfish4/bin/asadmin disable portalApi
 /Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue CellQueue
 /Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue FieldQueue
 /Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue LinkQueue
 /Volumes/Octant/glassfish4/bin/asadmin flush-jmsdest --desttype queue PortalQueue
+/Volumes/Octant/glassfish4/bin/asadmin enable portalApi

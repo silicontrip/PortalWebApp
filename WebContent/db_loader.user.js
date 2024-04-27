6fcd94b4f4558254578873ab7706dcd28992d2fb
Mark Heath <silicontrip@gmail.com>
2024-04-27 16:49:30 +1000

    backend javascript and loader IITC javascript

---
+++ WebContent/db_loader.user.js
@@ -0,0 +1,112 @@
+// ==UserScript==
+// @id             iitc-plugin-db-loader@silicontrip
+// @name           IITC Plugin: DB technology journey
+// @category       layer
+// @version        0.1.1
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @description
+// @updateURL      https://quadrant.silicontrip.net:8181/portalApi/db_loader.user.js
+// @downloadURL    https://quadrant.silicontrip.net:8181/portalApi/db_loader.user.js
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
+
+function wrapper(plugin_info) {
+
+	// ensure plugin framework is there, even if iitc is not yet loaded
+	if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+	//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+	//(leaving them in place might break the 'About IITC' page or break update checks)
+	plugin_info.buildName = 'hrabbit';
+	plugin_info.dateTimeVersion = '20140925.0037';
+	plugin_info.pluginId = 'db_loader';
+	//END PLUGIN AUTHORS NOTE
+
+	// use own namespace for plugin
+
+	window.plugin.dbLoader = {
+		hz_loader_url: "http://localhost:8080/portalApi/loader",
+        alertLock: false,
+		setupCSS: function() {
+			window.$("<style>").prop("type", "text/css").html(''+
+				'#hz_portal_grabber a {'+
+				'margin-left: 5px;'+
+				'margin-right: 5px;'+
+				'white-space: nowrap;'+
+				'display: inline-block;'+
+				'}'
+			).appendTo("head");
+		},
+		setup: function() {
+			console.log('dbLoader::setup');
+
+            window.PLAYER.apikey = window.plugin.dbLoader.getKey();
+            var keyMessage="Update Key";
+            var hasKey = true;
+            if (window.PLAYER.apikey == null) {
+                keyMessage="Enter Your API Key";
+                hasKey=false;
+            }
+
+			window.$('#sidebar').append('<div id="hz_portal_grabber" style="padding: 5px; font-size: 12px">'+
+			'<a onclick="window.plugin.dbLoader.addKey();return false;">'+keyMessage+'</a>'+
+			'</div>'
+			);
+            if (hasKey) {
+                window.$.post(window.plugin.dbLoader.hz_loader_url, { apikey: window.plugin.dbLoader.getKey(), agent: window.PLAYER.nickname }, window.plugin.dbLoader.loader).fail(window.plugin.dbLoader.fail);
+            }
+		},
+	addKey: function()
+	{
+		var portalgrabber_key = prompt('Technology Journey - Access key');
+		if(portalgrabber_key != null)
+		{
+			localStorage.setItem('net.silicontrip.ingress.apikey', portalgrabber_key);
+			console.log("portalGrabber - Setting key: " + portalgrabber_key);
+            // change sidebar key message
+            window.$("#hz_portal_grabber").html('<a onclick="window.plugin.dbLoader.addKey();return false;">Update Key</a>');
+		}
+	},
+	getKey: function()
+	{
+		var portalgrabber_key = localStorage.getItem('net.silicontrip.ingress.apikey');
+		console.log("portalGrabber - Getting key: " + portalgrabber_key);
+		return portalgrabber_key;
+	},
+    loader: function(script)
+    {
+        eval(script); // surely there's a better way
+    },
+        fail: function(xhr,status,error)
+        {
+            console.log("FAIL");
+            console.log(xhr);
+            console.log(status);
+            //console.log(error);
+
+        //    alert ("FAIL: " + JSON.stringify(xhr) + " " + status + " " + error);
+        }
+};
+
+
+	var setup = window.plugin.dbLoader.setup;
+
+	setup.info = plugin_info; //add the script info data to the function as a property
+	if(!window.bootPlugins) window.bootPlugins = [];
+	window.bootPlugins.push(setup);
+	// if IITC has already booted, immediately run the 'setup' function
+	if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
+
---
+++ userscripts/db_portal.js
@@ -0,0 +1,162 @@
+	window.plugin.dbPortalGrabber = {
+		hz_submit_url: window.silicontrip_ingress_url+"submitEntity",
+		hz_get_url: window.silicontrip_ingress_url+"getPortals",
+		hz_portals: [],
+		STROKE_STYLE: {
+			stroke: true,
+			opacity: 1,
+			weight: 0.5,
+			fill: true,
+			fillColor: null, //same as color by default
+			fillOpacity: 0.4,
+			clickable: true,
+		},
+		dbPortals: {},
+		portalLayer: {},
+		layer: null,
+		setupCSS: function() {
+			window.$("<style>").prop("type", "text/css").html(''
+			+'#hz_portal_grabber a {'
+			+'margin-left: 5px;'
+			+'margin-right: 5px;'
+			+'white-space: nowrap;'
+			+'display: inline-block;'
+			+'}'
+			).appendTo("head");
+		},
+		setup: function() {
+			console.log('dbPortalGrabber::setup');
+			console.log('this is a change. this is a change');
+
+			window.plugin.dbPortalGrabber.layer = new window.L.LayerGroup();
+			window.addLayerGroup('DB Portals', window.plugin.dbPortalGrabber.layer, true);
+			window.plugin.dbPortalGrabber.STROKE_STYLE.color = localStorage.getItem("window.plugin.dbPortalGrabber.STROKE_STYLE.color");
+
+			window.addHook('portalAdded', window.plugin.dbPortalGrabber.portalAdded);
+			window.addHook('mapDataRefreshEnd', window.plugin.dbPortalGrabber.pushPortals);
+			window.addHook('mapDataRefreshStart', window.plugin.dbPortalGrabber.mapDataRefreshStart);
+			// want to catch enable db portals layer. perform redraw.
+
+			window.$('#toolbox').append('<a onclick="window.plugin.dbPortalGrabber.portalColour();return false;" accesskey="C" >DB Colour</a>');
+
+		window.$('#hz_portal_grabber').html('<strong>Portal post</strong> '
+					+'<a onclick="window.plugin.dbPortalGrabber.addKey();return false;">Update key</a>'
+					+' (<span id="hz_portals">0</span>)');
+		},
+		portalAdded: function(data)
+		{
+		//	console.log (">>> portalAdded");
+
+			//var portal = data.portal.options.data;
+			var portal={};
+			var elements =  Object.getOwnPropertyNames(data.portal.options.data);
+			for (var ekey in elements)
+			{
+				var key = elements[ekey];
+				// console.log( key + ": " + data.portal.options.data[key]);
+				portal[key] = data.portal.options.data[key];
+			}
+			portal["guid"] = data.portal.options.guid;
+
+			window.plugin.dbPortalGrabber.hz_portals.push(portal);
+			window.$('#hz_portals').html(window.plugin.dbPortalGrabber.hz_portals.length);
+			//console.log ("<<< portalAdded");
+		},
+		selectPortal: function(e){
+			window.renderPortalDetails (e.target.guid);
+		},
+		portalColour: function() {
+			var html = '<div id="db_colour"><input id="colour_selector" type="color" name="color" value="' + window.plugin.dbPortalGrabber.STROKE_STYLE.color +'" /></div>' ;
+						  window.dialog({
+							html: html,
+							id: 'plugin-dbportal-colour',
+							dialogClass: 'ui-dialog-dbportal',
+							title: 'DB Portal Colour'
+						  });
+						document.getElementById('colour_selector').addEventListener('change', window.plugin.dbPortalGrabber.changeColour, false);
+		},
+		changeColour: function(evt) {
+			window.plugin.dbPortalGrabber.STROKE_STYLE.color = evt.srcElement.value;
+			localStorage.setItem("window.plugin.dbPortalGrabber.STROKE_STYLE.color", window.plugin.dbPortalGrabber.STROKE_STYLE.color);
+			window.plugin.dbPortalGrabber.portalLayer = {};
+			window.plugin.dbPortalGrabber.drawPortals();
+		},
+		mapDataRefreshStart: function()
+		{
+			window.$('#hz_portal_grabber').css('background-color', '');
+			if (!window.map.hasLayer(window.plugin.dbPortalGrabber.layer)) { return; }
+
+			var bounds = window.map.getBounds();
+			var alat = bounds._southWest.lat;
+			var alng = bounds._southWest.lng;
+			var blat = bounds._northEast.lat;
+			var blng = bounds._northEast.lng;
+
+			var query = "?ll=" + alat + "," + alng +"&l2=" + blat + "," + blng + "&agent=" + window.PLAYER.nickname ;
+			var geturl = window.plugin.dbPortalGrabber.hz_get_url+query;
+			//console.log(geturl);
+
+			window.$.get(geturl, window.plugin.dbPortalGrabber.loadJSONPortals);
+		},
+		loadJSONPortals: function (dbPortals) {
+			//console.log("dbPortalGrabber::loadJSONPortals");
+			window.plugin.dbPortalGrabber.dbPortals = dbPortals;
+			window.plugin.dbPortalGrabber.drawPortals();
+		},
+		drawPortals: function(){
+			//console.log("dbPortalGrabber::drawPortals");
+			// don't draw if all portals are visible.
+			if(window.map.getZoom() < 15) {
+				for (var pt in window.plugin.dbPortalGrabber.dbPortals)
+				{
+					//console.log(pt)
+					var thisPortal = window.plugin.dbPortalGrabber.dbPortals[pt];
+					if (! (pt in window.plugin.dbPortalGrabber.portalLayer)) {
+						var ll = window.L.latLng(thisPortal.lat/1000000.0, thisPortal.lng/1000000.0);
+						var circ = window.L.circle(ll,10,window.plugin.dbPortalGrabber.STROKE_STYLE);
+						circ.on('click', window.plugin.dbPortalGrabber.selectPortal);
+						circ.guid = pt;
+						window.plugin.dbPortalGrabber.portalLayer[pt] = circ;
+						circ.addTo(window.plugin.dbPortalGrabber.layer);
+					}
+				}
+			}
+			//console.log(window.plugin.dbPortals.layer);
+		},
+		pushPortals: function()
+		{
+			if(!window.plugin.dbPortalGrabber.hz_portals.length)
+			{
+				window.$('#hz_portal_grabber').css('background-color', 'green');
+				return;
+			}
+
+			// identify deleted portals
+			// check we are zoom 15 or higher
+			var deleted_portals =[];
+			//console.log("db_portal_grabber::pushPortals map zoom: "+window.map.getZoom());
+			if (window.map.getZoom() >= 15)
+			{
+				for (var guid in window.plugin.dbPortalGrabber.dbPortals)
+				{
+					// does this guid exist?
+					if (!window.portals.hasOwnProperty(guid)) //no?
+					{ deleted_portals.push({"delete": guid}); }
+				}
+				// add to deleted array
+			//console.log("db_portal_grabber::pushPortals deleted_portals: " +deleted_portals.length);
+			}
+			window.$('#hz_portals').html('Pushing.. '+window.plugin.dbPortalGrabber.hz_portals.length);
+			console.log('portalGrabber - Pushing portals - ' + window.plugin.dbPortalGrabber.hz_portals.length);
+			console.log(window.plugin.dbPortalGrabber.hz_portals);
+			window.$.post(window.plugin.dbPortalGrabber.hz_submit_url, {apikey: window.PLAYER.apikey, agent: window.PLAYER.nickname, portals: JSON.stringify(window.plugin.dbPortalGrabber.hz_portals), portals_deleted: JSON.stringify(deleted_portals)} )
+				.fail(function() {
+					alert("Technology Journey - error submitting portals");
+			});
+			window.plugin.dbPortalGrabber.hz_portals = [];
+			window.$('#hz_portal_grabber').css('background-color', 'green');
+			window.$('#hz_portals').html(window.plugin.dbPortalGrabber.hz_portals.length);
+		}
+	};
+
+window.plugin.dbPortalGrabber.setup();

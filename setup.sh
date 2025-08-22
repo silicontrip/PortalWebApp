31d56197c050683414cdc74c690a4c3b83aa78fe
Mark Heath <silicontrip@gmail.com>
2025-08-22 11:59:31 +1000

    fix up missing resource creation.

--- setup.sh
+++ setup.sh
@@ -6,11 +6,14 @@ $ASADMIN create-file-user --passwordfile /dev/stdin silicontrip <<EOF
 AS_ADMIN_USERPASSWORD=test123
 EOF
 
+$ASADMIN create-jdbc-resource --connectionpoolid DerbyPool jdbc/IngressResource
+
+
 $ASADMIN create-jms-resource --restype jakarta.jms.QueueConnectionFactory jms/QueueConnectionFactory
 
 $ASADMIN create-jms-resource --restype jakarta.jms.Queue jms/portalQueue
 $ASADMIN create-jms-resource --restype jakarta.jms.Queue jms/linkQueue
-$ASADMIN create-jms-resource --restype jakarta.jms.Queue jms/cellQueue
+$ASADMIN create-jms-resource --restype jakarta.jms.Queue jms/fieldQueue
 
 $ASADMIN delete-jvm-options -Xmx512m
 $ASADMIN create-jvm-options -Xmx4096m

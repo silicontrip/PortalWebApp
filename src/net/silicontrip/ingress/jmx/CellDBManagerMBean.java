6ff0fd8bdd160fbf8c1e58a0d8e34d01afa2492a
Mark Heath <silicontrip@gmail.com>
2025-11-23 00:12:47 +1100

    Possible future MBean jmx interfaces.

--- src/net/silicontrip/ingress/jmx/CellDBManagerMBean.java
+++ src/net/silicontrip/ingress/jmx/CellDBManagerMBean.java
@@ -8,6 +8,12 @@ public interface CellDBManagerMBean {
     // Method with a parameter
     public String traceCell(String cellIdToken);
 
+    //public String invalidate(String fieldGuid);
+    //public String eraseCells();
+    //public String rebuildCells();
+    //public String rebuildFieldCells();  // or is it cellFields
+    
+
     // Add any other methods you want to expose here...
     // public int getCachedCellCount();
     // public void clearCache();
